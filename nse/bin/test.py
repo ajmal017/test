b5e4638191fccbb4a9c5f4562c7fd8629141d8db
Abhishek <abhishek.chaturvedi@salesforce.com>
2017-08-13 04:16:57 +0530

    Aug 13

--- nse/bin/test.py
+++ nse/bin/test.py
@@ -23,12 +23,25 @@ class constants:
     nDays = 365
     pass
 
+def print_df(df):
 
+    print df
+    return True
 
 def main():
     parser = argparse.ArgumentParser(description='Future spread calculator')
     parser.add_argument('-s', '--stock', help='NSE stock symbol', required=True)
+    parser.add_argument('-l', '--lots', help='NSE stock symbol', required=True)
+    parser.add_argument('-z', '--size', help='NSE stock symbol', required=True)
     args = vars(parser.parse_args())
+
+    def print_profits():
+        print 'Points made:',diff_points
+        print 'Money Made:', round(profit,2)
+        print 'Percentage Gain:', pct_gain
+        print 'Trades:', trades
+        print merge.iloc[0]['Date']
+        print merge.iloc[-1]['Date']
 
     format = '%d%m%Y'
     format1 = '%d/%m/%y'
@@ -90,25 +103,27 @@ def main():
     #near['CME'] = pd.to_datetime(cme)
 
     #Calculate the Days to expiry datetime object
+    fulcrum = 'Open'
 
     current['DTE'] = current['Expiry'].apply(pd.to_datetime) - current['Date'].apply(pd.to_datetime)
     #Convert to number of days left to expiry
     current['DTE'] = current['DTE']/ np.timedelta64(1,'D')
-    current['fairprice'] = current['Underlying']*(1 + constants.rrate*(current['DTE']/constants.nDays)/100)
+    current['fairprice'] = current['Underlying'].shift(1)*(1 + constants.rrate*(current['DTE']/constants.nDays)/100)
     current.fillna(0,inplace=True)
     mid.fillna(0,inplace=True)
-    columns = ['Date','fairprice','current_price','middle_price','Spread_cm','DTE']
+    columns = ['Date','fairprice','current_price','middle_price','Spread','DTE']
     merge = pd.DataFrame(columns=columns)
     merge['Date']=current['Date']
     merge['DTE'] = current['DTE']
-    merge['Spread_cm'] = current['Settle Price'] - mid['Settle Price']
+    current['Spread'] = current[fulcrum] - mid[fulcrum]
+    merge['Spread'] = current['Spread']
     merge['fairprice'] = current['fairprice'].apply(lambda x: round(x,2))
-    merge['Diff'] = merge['fairprice'] - current['Settle Price']
+    merge['fairprice_Diff'] = merge['fairprice'] - current[fulcrum]
 
-    merge['current_price'] = current['Settle Price']
-    merge['middle_price'] = mid['Settle Price']
-    merge['mid_O'] = mid['Open']
-    merge['current_O'] = current['Open']
+    merge['current_price'] = current[fulcrum]
+    merge['middle_price'] = mid[fulcrum]
+    merge['mid_O'] = mid[fulcrum]
+    merge['current_O'] = current[fulcrum]
     merge['mid_H'] = mid['High']
     merge['current_H'] = current['High']
     merge['mid_L'] = mid['Low']
@@ -116,9 +131,86 @@ def main():
     merge['mid_OI'] = mid['Open Interest']
 
     merge.fillna(0,inplace=True)
-    print merge
+    merge.to_csv(directory+'/%s_merge.csv' % tick)
+    '''
+    if spread > 2:
+        buy mid and sell current
+        if open_spread is < 2:
+            sell mid and buy current
+    elif spread is < 2:
+        sell mid and buy current
+        if open_spread is > 2:
+            buy mid and sell current
+    '''
+    mid_buy = 0
+    mid_sell = 0
+    current_buy = 0
+    current_sell = 0
+    contago = False
+    backwardation = False
+    bullish = False
+    bearish = False
+    in_market = False
+    trades = 0
+    threshold_upper = 1.5
+    threshold_lower = -1.0
+    for i, row in merge.iterrows():
+        if in_market:
+            if row['Spread'] > threshold_upper:
+                mid_buy = mid_buy + row['mid_O']
+                current_sell = current_sell + row['current_O']
+                in_market = False
+                print 'Closing the transaction:'
+                print '\t%s->%s->%s:%s' %(row['Date'],row['Spread'],mid_buy,current_sell)
+
+            if row['Spread'] < threshold_lower:
+                mid_sell = mid_sell + row['mid_O']
+                current_buy = current_buy + row['current_O']
+                in_market = False
+                print 'Closing the transaction:'
+                print '\t%s->%s->%s:%s' %(row['Date'],row['Spread'],mid_sell,current_buy)
+
+        else:
+            if row['Spread'] > threshold_upper:
+                contago = True
+                mid_buy = mid_buy + row['mid_O']
+                current_sell = current_sell + row['current_O']
+                in_market = True
+                trades = + 1
+                print 'Spread is positive:'
+                print '\t%s->%s->%s:%s' %(row['Date'],row['Spread'],mid_buy,current_sell)
+
+                continue
+            if row['Spread'] < threshold_lower:
+                backwardation = True
+                mid_sell = mid_sell + row['mid_O']
+                current_buy = current_buy + row['current_O']
+                in_market = True
+                trades = +1
+                print 'Spread is negative:'
+                print '\t%s->%s->%s:%s' %(row['Date'],row['Spread'],mid_sell,current_buy)
+                continue
+    print mid_buy, mid_sell, current_sell, current_buy
 
 
+    """
+    for index, row in merge.iterrows():
+        if (merge[contago]):
+            mid_buy = + mid.Open
+            current_sell = + current.Open
+        if (merge[backwardation]):
+            mid_sell = + mid.Open
+            current_buy = + current.Open
+    """
+    diff_points = (mid_sell - mid_buy) + (current_sell - current_buy)
+    lots = float(args['lots'])
+    lot_size = float(args['size'])
+    profit = lots * diff_points * lot_size
+    margin = 30000
+    pct_gain = (profit - margin)/margin
+    print_profits()
+    print_df(merge[['current_O','mid_O','Spread','mid_OI']])
+    print(merge['Spread'].mean)
     #expiry_cal = ['27012017','23022017','30032017','27042017']
 
 
